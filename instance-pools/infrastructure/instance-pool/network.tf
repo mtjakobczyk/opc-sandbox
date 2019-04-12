6ae771e85b6d4a09c0ce62d2c7d99ba94c62a488
Michal Jakobczyk <mtjakobczyk@gmail.com>
2019-04-12 13:24:12 +0200

    Create network.tf

---
+++ instance-pools/infrastructure/instance-pool/network.tf
@@ -0,0 +1,32 @@
+# ipool module - networking.tf
+
+resource "opc_compute_ip_address_reservation" "ip_rsv_public" {
+  count = "${var.instance_count}"
+  name            = "${var.instance_prefix}${count.index + 1}-eth0-ip-rsv-public"
+  ip_address_pool = "public-ippool"
+}
+resource "opc_compute_ip_address_reservation" "ip_rsv_cloud" {
+  count = "${var.instance_count}"
+  name            = "${var.instance_prefix}${count.index + 1}-eth0-ip-rsv-cloud"
+  ip_address_pool = "cloud-ippool"
+}
+
+resource "opc_compute_ip_address_association" "ip_asso_public" {
+  count = "${var.instance_count}"
+  name                   = "${var.instance_prefix}${count.index + 1}-eth0-ip-asso-public"
+  ip_address_reservation = "${opc_compute_ip_address_reservation.ip_rsv_public.*.name[count.index]}"
+  vnic                   = "${var.instance_prefix}${count.index + 1}-eth0"
+  depends_on = [ "opc_compute_instance.inst" ]
+}
+resource "opc_compute_ip_address_association" "ip_asso_cloud" {
+  count = "${var.instance_count}"
+  name                   = "${var.instance_prefix}${count.index + 1}-eth0-ip-asso-cloud"
+  ip_address_reservation = "${opc_compute_ip_address_reservation.ip_rsv_cloud.*.name[count.index]}"
+  vnic                   = "${var.instance_prefix}${count.index + 1}-eth0"
+  depends_on = [ "opc_compute_instance.inst" ]
+}
+
+# Outputs
+output "instance_public_ips" {
+  value = "${opc_compute_ip_address_reservation.ip_rsv_public.*.ip_address}"
+}
