ae8daf0abe620f751f1a27f7b363481466560f21
Michal Jakobczyk <mtjakobczyk@gmail.com>
2019-04-16 14:16:15 +0200

    Update compute.tf

--- simple-vm/infrastructure/compute.tf
+++ simple-vm/infrastructure/compute.tf
@@ -11,8 +11,8 @@ resource "opc_compute_instance" "simplevm" {
   networking_info {
     index          = 0
     vnic = "sandbox-vm-eth0"
-    vnic_sets = [ "${opc_compute_vnic_set.ipnet_vnics.name}" ]
-    ip_network     = "${opc_compute_ip_network.ipnet.name}"
+    vnic_sets = [ "/Compute-123456789/second.user@example.com/sandbox-ip-network-vnics" ]
+    ip_network     = "/Compute-123456789/second.user@example.com/sandbox-ip-network"
     shared_network = false
     dns = [ "sandboxvm.example.com" ]
     name_servers = [  ]
@@ -29,7 +29,7 @@ resource "opc_compute_storage_volume" "simplevm_bootvolume" {
   name        = "sandbox-vm-volume"
   size        = 15
   bootable    = true
-  image_list  = "/oracle/public/OL_7.2_UEKR4_x86_64"
+  image_list  = "/Compute-123456789/first.user@example.com/OL76_UEKR5"
   image_list_entry = 1
 }
 
